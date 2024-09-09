80459ae523620c5be5fb49d8b2be242b9d9c0c38
subugee <sasubramanian_md@hotmail.com>
2024-09-09 10:26:24 +0530

    db app

--- db-app/src/app/api/users/route.js
+++ db-app/src/app/api/users/route.js
@@ -2,7 +2,7 @@ import { prisma } from "@/app/lib/prisma";
 import { NextResponse } from "next/server";
 
 //get all users 
-export  async function GET() {
+export async function GET() {
 
     try {
         const users = await prisma.user.findMany()
@@ -16,5 +16,22 @@ export  async function GET() {
         return NextResponse.json({ err })
     }
 
-
+}
+//create 
+export async function POST(request) {
+    try {
+        const user = await request.json()
+        const response = await prisma.user.create({
+            data: user
+        })
+        //
+        return new NextResponse(JSON.stringify(response), {
+            status: 201, headers: {
+                "Content-Type": "application/json"
+            }
+        })
+    }
+    catch (err) {
+        return NextResponse.json({ err })
+    }
 }
